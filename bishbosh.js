12a1aa18ae11168849c203675bd9ca8465005399
jarethholt <jareth.holt@gmail.com>
2024-04-04 09:44:36 +0200

    Edited bishboshv2 to satisfy actual exercise specification

--- bishbosh.js
+++ bishbosh.js
@@ -1,3 +1,18 @@
+// Constants
+const bishboshForm = document.getElementById('bishboshform')
+const bishField = document.getElementById('bish')
+const boshField = document.getElementById('bosh')
+const maxField = document.getElementById('max')
+const runButton = document.getElementById('run')
+const outputDiv = document.getElementById('output')
+
+// Make sure page doesn't reload when hitting the run button
+function handleForm(event) {
+    event.preventDefault()
+}
+bishboshForm.addEventListener('submit', handleForm)
+
+// Bish-bosh calculation for a single value
 function bishbosh_single(bish, bosh, value) {
     let result = ''
     if (value % bish == 0) {
@@ -15,6 +30,7 @@ function bishbosh_single(bish, bosh, value) {
     return result
 }
 
+// Generate the bish-bosh HTML for a range of values
 function bishbosh_full(bish, bosh, max) {
     let html = `<ul>`
     for (let value = 1; value <= max; value++) {
@@ -23,3 +39,15 @@ function bishbosh_full(bish, bosh, max) {
     html += `</ul>`
     return html;
 }
+
+// Run the bish-bosh function with page interaction
+function runBishBosh() {
+    let bish = bishField.value;
+    let bosh = boshField.value;
+    let max = maxField.value;
+    let html = bishbosh_full(bish, bosh, max)
+    outputDiv.innerHTML = html
+}
+
+// Set the form to run bish-bosh when run is clicked
+bishboshForm.setAttribute('onsubmit', 'runBishBosh()')
