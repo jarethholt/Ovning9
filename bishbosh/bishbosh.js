d1b28c98ba33ce08134993180a81c3f379d7581b
jarethholt <jareth.holt@gmail.com>
2024-04-05 14:57:42 +0200

    Minor modifications to javascript, more comments

--- bishbosh/bishbosh.js
+++ bishbosh/bishbosh.js
@@ -1,20 +1,24 @@
-// Constants
-const bishboshForm = document.getElementById('bishboshform')
-const bishField = document.getElementById('bish')
-const boshField = document.getElementById('bosh')
-const maxField = document.getElementById('max')
-const runButton = document.getElementById('run')
-const outputDiv = document.getElementById('output')
+'use strict';
+
+// Global variables
+const app = {
+    bishboshForm: document.getElementById('bishboshform'),
+    bishField   : document.getElementById('bish'),
+    boshField   : document.getElementById('bosh'),
+    maxField    : document.getElementById('max'),
+    runButton   : document.getElementById('run'),
+    outputDiv   : document.getElementById('output'),
+}
 
 // Make sure page doesn't reload when hitting the run button
 // function handleForm(event) {
 //     event.preventDefault()
 // }
 // bishboshForm.addEventListener('submit', handleForm)
-bishboshForm.addEventListener('submit', event => event.preventDefault())
+app.bishboshForm.addEventListener('submit', event => event.preventDefault())
 
 // Set the form to run bish-bosh when run is clicked
-bishboshForm.setAttribute('onsubmit', 'runBishBosh()')
+app.bishboshForm.setAttribute('onsubmit', 'runBishBosh()')
 
 // Bish-bosh calculation for a single value
 function bishbosh_single(bish, bosh, value) {
@@ -46,9 +50,9 @@ function bishbosh_full(bish, bosh, max) {
 
 // Run the bish-bosh function with page interaction
 function runBishBosh() {
-    let bish = bishField.value
-    let bosh = boshField.value
-    let max = maxField.value
-    let html = bishbosh_full(bish, bosh, max)
-    outputDiv.innerHTML = html
+    const bish = app.bishField.value
+    const bosh = app.boshField.value
+    const max  = app.maxField.value
+    const html = bishbosh_full(bish, bosh, max)
+    app.outputDiv.innerHTML = html
 }
--- todolist/todolist.js
+++ todolist/todolist.js
@@ -1,66 +1,75 @@
-// Constants
-const addItemForm = document.getElementById('addItemForm')
-const itemField = document.getElementById('itemToAdd')
-const addItemButton = document.getElementById('addItem')
-const clearListButton = document.getElementById('clearList')
-const currentListDiv = document.getElementById('currentList')
+'use strict';
 
-// Global variable: index of current item
-let currItemIndex = 0
+// Global variables
+const app = {
+    addItemForm    : document.getElementById('addItemForm'),
+    itemField      : document.getElementById('itemToAdd'),
+    addItemButton  : document.getElementById('addItem'),
+    clearListButton: document.getElementById('clearList'),
+    currentListDiv : document.getElementById('currentList'),
+    currItemIndex  : 0
+}
+
+// Define which functions are attached to which buttons
+app.addItemForm.setAttribute('onsubmit', 'submitItem()')
+app.clearListButton.setAttribute('onclick', 'clearList()')
 
 // Make sure page doesn't reload when hitting submit button
-addItemForm.addEventListener('submit', event => event.preventDefault())
+app.addItemForm.addEventListener('submit', event => event.preventDefault())
 
 // Set the form to add the item when submitted
 function submitItem() {
-    let item = itemField.value
+    const item = app.itemField.value
     addItem(item)
     addItemForm.reset()
-    return false
 }
-addItemForm.setAttribute('onsubmit', 'submitItem()')
 
 // Implement the button to clear the current list
 function clearList() {
+    // Use a popup window to confirm
     if (confirm('Do you really want to clear this list?')) {
-        currentListDiv.innerHTML = ''
-        currItemIndex = 0
+        app.currentListDiv.innerHTML = ''
+        app.currItemIndex = 0
     }
 }
-clearListButton.setAttribute('onclick', 'clearList()')
 
 // Create the row HTML for an item
 function createItemRow(item) {
-    let itemName = `Item${currItemIndex}`
+    const itemName = `Item${app.currItemIndex}`
+    // Set up a div with a checkbox, text, and "remove item" button
     let html = `<div class="toDoListItemDiv" id="div${itemName}">`
+    // The checkbox runs checkItem('${itemName}') to deal with toggling
     html += `<input type="checkbox" class="checkbox" id="checkbox${itemName}" name="${itemName}" onclick="checkItem('${itemName}')">`
     html += `<p class="toDoListItem" id="${itemName}">${item}</p>`
+    // The remove item button runs removeItem('${itemName}')
     html += `<button type="button" class="removeItem" id="remove${itemName}" onclick="removeItem('${itemName}')">Remove</button>`
     html += '</div>'
     // Update the index to use for the next item
-    currItemIndex++
+    app.currItemIndex++
     return html
 }
 
 // Add an item to the current list
 function addItem(item) {
-    let rowHTML = createItemRow(item)
-    let currentListHTML = currentListDiv.innerHTML
+    const rowHTML = createItemRow(item)
+    const currentListHTML = app.currentListDiv.innerHTML
     // Add the row at the top of the list
-    currentListDiv.innerHTML = rowHTML + currentListHTML
+    app.currentListDiv.innerHTML = rowHTML + currentListHTML
 }
 
 // Remove an item based on its name
 function removeItem(itemName) {
-    let itemDiv = document.getElementById(`div${itemName}`)
+    const itemDiv = document.getElementById(`div${itemName}`)
     itemDiv.remove()
 }
 
 // Deal with the checkbox toggling
 function checkItem(itemName) {
-    let checkbox = document.getElementById(`checkbox${itemName}`)
-    let itemDiv = document.getElementById(`div${itemName}`)
-    let ptext = document.getElementById(itemName)
+    const checkbox = document.getElementById(`checkbox${itemName}`)
+    const itemDiv = document.getElementById(`div${itemName}`)
+    const ptext = document.getElementById(itemName)
+    // The "checked" class is defined in the CSS
+    // Changes the background color of the div and strikes through the text
     itemDiv.classList.toggle("checked", checkbox.checked)
     ptext.classList.toggle("checked", checkbox.checked)
 }
