98e2f3f68806ba61c40b1a801ed3628e83b35602
jarethholt <jareth.holt@gmail.com>
2024-04-08 13:30:22 +0200

    Make better use of event handlers and 'this'

--- todolist/todolist.js
+++ todolist/todolist.js
@@ -2,12 +2,11 @@
 
 // Global variables
 const app = {
-  addItemForm: document.getElementById('addItemForm'),
-  itemField: document.getElementById('itemToAdd'),
-  addItemButton: document.getElementById('addItem'),
-  clearListButton: document.getElementById('clearList'),
-  currentListDiv: document.getElementById('currentList'),
-  currItemIndex: 0
+  addItemForm: document.querySelector('#addItemForm'),
+  itemField: document.querySelector('#itemToAdd'),
+  addItemButton: document.querySelector('#addItem'),
+  clearListButton: document.querySelector('#clearList'),
+  currentListDiv: document.querySelector('#currentList')
 }
 
 // Define which functions are attached to which buttons
@@ -15,7 +14,8 @@ app.addItemForm.setAttribute('onsubmit', 'submitItem()')
 app.clearListButton.setAttribute('onclick', 'clearList()')
 
 // Make sure page doesn't reload when hitting submit button
-app.addItemForm.addEventListener('submit', event => event.preventDefault())
+app.addItemForm.addEventListener('submit',
+  event => event.preventDefault())
 
 // Set the form to add the item when submitted
 function submitItem() {
@@ -24,52 +24,54 @@ function submitItem() {
   addItemForm.reset()
 }
 
+function addItem(item) {
+  // Create a div element for the new item
+  const listItemDiv = document.createElement('div')
+  listItemDiv.classList.add('toDoListItemDiv')
+  // Design the checkbox for the item
+  const checkbox = document.createElement('input')
+  checkbox.type = 'checkbox'
+  checkbox.name = 'checkbox'
+  checkbox.addEventListener('change', checkItem)
+  // Add text for the item
+  const ptext = document.createElement('p')
+  ptext.classList.add('toDoListItem')
+  ptext.append(item)
+  // Add a button to remove the item
+  const removeButton = document.createElement('button')
+  removeButton.type = 'button'
+  removeButton.classList.add('removeItem')
+  removeButton.append('Remove')
+  removeButton.addEventListener('click', removeItem)
+  // Combine all elements into the parent div
+  listItemDiv.append(checkbox, ptext, removeButton)
+  // Prepend the parent div to the currentListDiv
+  app.currentListDiv.prepend(listItemDiv)
+}
+
+// Remove an item based on its name
+function removeItem() {
+  // Get the parent of the remove button
+  const itemDiv = this.parentElement;
+  // Remove the entire div
+  itemDiv.remove()
+}
+
+// Deal with the checkbox toggling
+function checkItem() {
+  // Get the parent of the checkbox
+  const itemDiv = this.parentElement;
+  // Add the style class "checked"
+  itemDiv.classList.toggle("checked", this.checked)
+  // Get the sibling text of the checkbox and add style class "checked"
+  const ptext = itemDiv.querySelector('p')
+  ptext.classList.toggle("checked", this.checked)
+}
+
 // Implement the button to clear the current list
 function clearList() {
   // Use a popup window to confirm
   if (confirm('Do you really want to clear this list?')) {
     app.currentListDiv.innerHTML = ''
-    app.currItemIndex = 0
   }
-}
-
-// Create the row HTML for an item
-function createItemRow(item) {
-  const itemName = `Item${app.currItemIndex}`
-  // Set up a div with a checkbox, text, and "remove item" button
-  let html = `<div class="toDoListItemDiv" id="div${itemName}">`
-  // The checkbox runs checkItem('${itemName}') to deal with toggling
-  html += `<input type="checkbox" class="checkbox" id="checkbox${itemName}" name="${itemName}" onclick="checkItem('${itemName}')">`
-  html += `<p class="toDoListItem" id="${itemName}">${item}</p>`
-  // The remove item button runs removeItem('${itemName}')
-  html += `<button type="button" class="removeItem" id="remove${itemName}" onclick="removeItem('${itemName}')">Remove</button>`
-  html += '</div>'
-  // Update the index to use for the next item
-  app.currItemIndex++
-  return html
-}
-
-// Add an item to the current list
-function addItem(item) {
-  const rowHTML = createItemRow(item)
-  const currentListHTML = app.currentListDiv.innerHTML
-  // Add the row at the top of the list
-  app.currentListDiv.innerHTML = rowHTML + currentListHTML
-}
-
-// Remove an item based on its name
-function removeItem(itemName) {
-  const itemDiv = document.getElementById(`div${itemName}`)
-  itemDiv.remove()
-}
-
-// Deal with the checkbox toggling
-function checkItem(itemName) {
-  const checkbox = document.getElementById(`checkbox${itemName}`)
-  const itemDiv = document.getElementById(`div${itemName}`)
-  const ptext = document.getElementById(itemName)
-  // The "checked" class is defined in the CSS
-  // Changes the background color of the div and strikes through the text
-  itemDiv.classList.toggle("checked", checkbox.checked)
-  ptext.classList.toggle("checked", checkbox.checked)
 }
